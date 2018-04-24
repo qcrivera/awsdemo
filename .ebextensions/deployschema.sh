1fbc88ec7c242ae54512cbc195c78487020e4f44
Carlos Rivera <crivera@bell.net>
2018-04-24 00:04:42 +0000

    this

---
+++ .ebextensions/deployschema.sh
@@ -0,0 +1,13 @@
+#!/bin/sh\
+/usr/bin/mysql \
+    -u $RDS_USERNAME \
+    -p$RDS_PASSWORD \
+    -h $RDS_HOSTNAME \
+#    $RDS_DB_NAME \
+    -e 'CREATE DATABASE IF NOT EXISTS ebdb'
+/usr/bin/mysql \
+    -u $RDS_USERNAME \
+    -p$RDS_PASSWORD \
+    -h $RDS_HOSTNAME \
+    $RDS_DB_NAME \
+    -e 'CREATE TABLE IF NOT EXISTS urler(id INT UNSIGNED NOT NULL AUTO_INCREMENT, author VARCHAR(63) NOT NULL, message TEXT, PRIMARY KEY (id))'
