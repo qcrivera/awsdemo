1c9422e34056b69303f2392ec3b76dfafb735133
carlos <crivera@bell.net>
2018-04-23 20:34:22 -0400

    fix syntax error

--- .ebextensions/deployschema.sh
+++ .ebextensions/deployschema.sh
@@ -1,9 +1,8 @@
-#!/bin/sh\
+#!/bin/sh
 /usr/bin/mysql \
     -u $RDS_USERNAME \
     -p$RDS_PASSWORD \
     -h $RDS_HOSTNAME \
-#    $RDS_DB_NAME \
     -e 'CREATE DATABASE IF NOT EXISTS ebdb'
 /usr/bin/mysql \
     -u $RDS_USERNAME \
